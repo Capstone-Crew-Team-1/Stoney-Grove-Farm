9f79fd4ed842913d4364835ec7214ddd19ec4ebf
Alex A <alperta@ferris.edu>
2020-02-23 15:49:11 -0500

    Big Update. Added blog page to nav, updated index page, added client pics to images, placed scripts for recipe cards into custom.js folder

---
+++ stoney-grove-farms/js/custom.js
@@ -0,0 +1,32 @@
+
+<!--Popover that show list of ingredients-->
+
+$(document).ready(function () {
+    $('.btn1').popover({
+        title: 'Ingredients',
+        content:
+            '1 stick unsalted butter. 1 teaspoon cinnamon sugar. 2 tablespoons granulated sugar. Pinch salt. 4 ripe peaches, halved and pitted. canola oil. Mint leaves, for garnish.',
+        selector: '.btn',
+        trigger: 'focus'
+    });
+    $('.btn2').popover({
+        title: 'Ingredients',
+        content: 'Eggs. Sugar. Bread flour. Honey',
+        selector: '.btn',
+        trigger: 'focus'
+    });
+    $('.btn3').popover({
+        title: 'Ingredients',
+        content:
+            '1 (16 oz) bag pretzels. 1/4 cup butter melted. 1/4 cup honey. 3 Tbsp Dijon mustard. 1 tsp onion powder. 1/2 tsp garlic powder. 2 tsp dried mustard',
+        selector: '.btn',
+        trigger: 'focus'
+    });
+
+    <!--Handles card flip-->
+    $('.card').flip();
+});
+
+
+
+
