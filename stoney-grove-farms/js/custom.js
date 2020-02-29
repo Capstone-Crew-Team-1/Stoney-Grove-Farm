c2543a9a656d551d7b6381b58995daa6784f7be4
Alex A <alperta@ferris.edu>
2020-02-29 00:52:45 -0500

    Image Gallery to home page
    Added the owlCarousel code for the image gallery

--- stoney-grove-farms/js/custom.js
+++ stoney-grove-farms/js/custom.js
@@ -29,7 +29,11 @@ $(document).ready(function () {
     <!--Contact Modal-->
     $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').trigger('focus')
-    })
+    });
+
+    // manual carousel controls
+    $('.next').click(function(){ $('.carousel').carousel('next');return false; });
+    $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
 });
 
 
